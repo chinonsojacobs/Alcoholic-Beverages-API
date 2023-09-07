a5a782d03bfddc25e8b0e0b8c3b528ddab3beddf
Chinonso Jesse Okoronkwo <chinonsojacobs@gmail.com>
2023-09-07 04:10:23 +0100

    Tiny changes

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require('dotenv').config({ path: 'ENV_FILENAME' });
 
 const DBconnection = async() => {
     const con = await mongoose.connect(process.env.MONGO_URI, {
